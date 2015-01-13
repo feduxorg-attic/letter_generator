e5a37029302dcf3e7d03a43ac2ac61639d698207
Max Meyer <dev@fedux.org>
2015-01-14 00:04:04 +0100

    Added first tests

--- features/generate_letter.feature
+++ features/generate_letter.feature
@@ -1,6 +1,10 @@
 Feature: Generate Letter
 
+  Background:
+    Given the default letter template
+
+
   Scenario: Non-existing letter
     Given a letter named "letter" does not exist
     When I successfully run `letter_generator generate`
-    Then a letter named "letter" exist
+    Then a letter named "letter" should exist
--- features/step_definitions.rb
+++ features/step_definitions.rb
@@ -5,5 +5,17 @@ Given(/^a letter named "(.*?)" does not exist$/) do |name|
 end
 
 Then(/^a letter named "(.*?)" should exist$/) do |name|
-  step %(a filed named "#{name}.tex" should exist)
+  step %(a file named "#{name}.tex" should exist)
+end
+
+Given(/^the default letter template$/) do
+  step %(a letter template named "letter" with:), File.read(File.expand_path('../../templates/letter.tex.tt', __FILE__))
+end
+
+Given(/^a letter template named "(.*?)" with:$/) do |name, string|
+  step %(a file named "~/.config/letter_generator/templates/#{name}.tex.tt" with:), string
+end
+
+Given(/^a config file named "(.*?)" with:$/) do |name, string|
+  step %(a file named "~/.config/letter_generator/#{name}" with:), string
 end
